2958adf8fe6419d63c4edaf68dd60ae82e13b0ac
ramesh6859 <ramesh6859@gmail.com>
2021-08-21 01:17:52 +0530

    Third Program second commit

--- Sum_and_Difference_of_Two_Numbers.c
+++ Sum_and_Difference_of_Two_Numbers.c
@@ -5,24 +5,38 @@ Language	:	C
 IDE		    :	Visual Studio
 Date		:	20/08/2021
 mail		:	rameshofficial75@gmail.com
-Expected Input	:	First Line : Two integers.
-                    Second Line : Two floating point numbers. 
-Expected Output	:	First Line : Print the sum and difference of 
-                    both integers.
-                    Second Line : Print the sum and difference of both float (scaled to decimal place) separated by a space on the second line. 
+Expected Input	:	First Line : First integer.
+                    Second Line : Second Integer.
+                    Third Line : First floating point number.
+                    Fourth Line : Second floating point number. 
+Expected Output	:	First Line : Sum of two integers.
+                    Second Line : Difference of two integers.
+                    Third Line : Sum of two floating point numbers.
+                    Fourth Line : Difference of two floating point numbers.
 */
 
 #include <stdio.h>
 int main()
 {
-    int a, b;
-    float c, d;
-    scanf("%d %d", &a, &b);
-    scanf(" ");
-    scanf("%f %f", &c, &d);
-    printf("%d %d\n", a+b, a-b);
-    printf("%.1f %.1f\n", c+d, c-d);
+    printf("\n");
+    
+    int first_integer, second_integer;
+    float first_float, second_float;
 
-    printf("\n\n");
+    printf("Enter first integer: \n");
+    scanf("%d", &first_integer);
+    printf("Enter second integer: \n");
+    scanf("%d", &second_integer);
+    printf("Enter first floating point number: \n");
+    scanf("%f", &first_float);
+    printf("Enter second floating point number: \n");
+    scanf("%f", &second_float);
+
+    printf("Sum of integers: %d\n", first_integer + second_integer);
+    printf("Difference of integers: %d\n", first_integer - second_integer);
+    printf("Sum of floating point numbers: %.2f\n", first_float + second_float);
+    printf("Difference of floating point numbers: %.2f\n", first_float - second_float);
+
+    printf("\n");
     return 0;
 }
